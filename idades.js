d1b0af1569522660796ddb7582ccf84d3863bd18
tutucardoso <arthur.reis0511@gmail.com>
2025-05-27 17:09:33 -0300

    idade.js

---
+++ idades.js
@@ -0,0 +1,10 @@
+// ler a idade do usúario
+// se idade 18 ou mais, dizer que é maior
+// senao, diz que é menor
+
+ let idade = prompt("qual sua idade?")
+if(idade >=18){
+alert("voce é maior")
+}else{
+    alert("voce é menor de idade.")
+}
